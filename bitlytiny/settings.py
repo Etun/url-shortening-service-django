14c67e93f82354d4698e2943f2eedab45751f3ed
Ellen Tunik <helentunik@gmail.com>
2015-06-05 19:59:13 +0300

    redirecting to original url added

--- bitlytiny/settings.py
+++ bitlytiny/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'api',
+
 )
 
 MIDDLEWARE_CLASSES = (
@@ -55,7 +57,9 @@ ROOT_URLCONF = 'bitlytiny.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- bitlytiny/urls.py
+++ bitlytiny/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from api.views import LinkCreate, LinkShow, RedirectToLongURL
+
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', LinkCreate.as_view(), name='home'),
+    url(r'^link/(?P<pk>\d+)$', LinkShow.as_view(), name='link_show'),
+    url(r'^r/(?P<short_url>\w+)$', RedirectToLongURL.as_view(),
+        name='redirect_short_url'),
 ]
